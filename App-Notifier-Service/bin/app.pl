edd9974fa2dad4a475614b7d080c7f1c7e908678
Shlomi Fish <shlomif@shlomifish.org>
2014-04-12 19:56:19 +0300

    Add SIGINT handling - Ctrl+C.

--- App-Notifier-Service/bin/app.pl
+++ App-Notifier-Service/bin/app.pl
@@ -1,4 +1,16 @@
 #!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+
 use Dancer;
 use App::Notifier::Service;
-dance;
+
+{
+    # Exit gracefully on Ctrl+C - Konsole warns about it.
+    local $SIG{INT} = sub {
+        exit(0);
+    };
+    dance;
+}
