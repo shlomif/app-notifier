9948e8f165fac159891c5c2934fc7ac66860ddc4
Shlomi Fish <shlomif@shlomifish.org>
2016-03-31 18:08:46 +0300

    Bump version number to 0.0300

--- App-Notifier-Client/lib/App/Notifier/Client.pm
+++ App-Notifier-Client/lib/App/Notifier/Client.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use vars qw($VERSION);
 
-$VERSION = '0.0204';
+$VERSION = '0.0300';
 
 use 5.012;
 
--- App-Notifier-Client/lib/App/Notifier/Client/Notifier_App.pm
+++ App-Notifier-Client/lib/App/Notifier/Client/Notifier_App.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use vars qw($VERSION);
 
-$VERSION = '0.0203';
+$VERSION = '0.0300';
 
 use 5.012;
 
