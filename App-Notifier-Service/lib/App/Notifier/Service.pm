a0f5d66803f44163a927b3537ca24d6e7a5140db
Shlomi Fish <shlomif@shlomifish.org>
2017-04-02 16:11:10 +0300

    reduce more

--- App-Notifier-Service/lib/App/Notifier/Service.pm
+++ App-Notifier-Service/lib/App/Notifier/Service.pm
@@ -6,9 +6,6 @@ use Dancer ':syntax';
 
 our $VERSION = '0.0400';
 
-use File::Spec;
-use YAML::XS qw( LoadFile );
-use JSON::MaybeXS qw(decode_json);
 use List::MoreUtils qw();
 
 use POSIX ":sys_wait_h";
@@ -22,38 +19,6 @@ sub _REAPER {
 }
 
 $SIG{CHLD} = \&_REAPER;
-
-my $config_fn = ($ENV{'NOTIFIER_CONFIG'}
-    || File::Spec->catfile($ENV{HOME}, '.app_notifier.yml'));
-
-my $config;
-
-sub _process_cmd_line_arg
-{
-    my ($arg, $text_params) = @_;
-
-    if (ref($arg) eq '')
-    {
-        return $arg;
-    }
-    elsif (ref($arg) eq 'HASH')
-    {
-        if ($arg->{type} eq 'text_param')
-        {
-            return ($text_params->{$arg->{param_name}} // '');
-        }
-        else
-        {
-            die +{ msg => "Unknown special argument type $arg->{type}", };
-        }
-    }
-    else
-    {
-        die +{ msg => "Unhandled perl type for argument in command line template (should be string or hash.", };
-    }
-
-    return;
-}
 
 get '/notify' => sub {
 
@@ -72,121 +37,5 @@ get '/notify' => sub {
     return "Success.\n";
 };
 
-get '/' => sub {
-    template 'index';
-};
-
 true;
 
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-App::Notifier::Service - an HTTP service for the notifier application for
-notifying that an event (such as the finish of a task) occured.
-
-=head1 SYNOPSIS
-
-    # Prepare a YAML file in ~/.app_notifier.yml with content like this:
-
-    $ cat <<EOF > ~/.app_notifier.yml
-    commands:
-        # These entries contain command lines that get invoked after the
-        # notification is received. They can be either strings or arrays.
-        default:
-            - /home/shlomif/bin/desktop-finish-cue
-        shine:
-            - /home/shlomif/bin/desktop-finish-cue
-            - "--song"
-            - "/home/music/Music/dosd-mp3s/Carmen and Camille - Shine 4U"
-            - "--message"
-            - type: "text_param"
-              param_name: "msg"
-    EOF
-
-    # Run the Dancer application from the distribution's root directory.
-    ./bin/app.pl
-
-    # Alternatively run the following Perl code:
-    use Dancer;
-    use App::Notifier::Service;
-    dance;
-
-    # When you want to notify that an event occured:
-    $ curl 'http://127.0.0.1:3000/notify'
-    $ curl 'http://127.0.0.1:3000/notify?cmd_id=shine'
-
-
-=head1 AUTHOR
-
-Shlomi Fish, L<http://www.shlomifish.org/> .
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-app-notifier-service at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Notifier-Service>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc App::Notifier::Service
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Notifier-Service>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-Notifier-Service>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/App-Notifier-Service>
-
-=item * MetaCPAN
-
-L<http://metacpan.org/release/App-Notifier-Service>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2012 Shlomi Fish.
-
-This program is distributed under the MIT (X11) License:
-L<http://www.opensource.org/licenses/mit-license.php>
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-=cut
