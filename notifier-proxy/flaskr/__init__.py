c7727495e6f2ccbe6ee4117cd530b2317e4f692c
Shlomi Fish <shlomif@shlomifish.org>
2020-04-11 19:42:46 +0300

    add customisations

--- notifier-proxy/flaskr/__init__.py
+++ notifier-proxy/flaskr/__init__.py
@@ -1,12 +1,18 @@
+import os
+
 from flask import Flask, request
 
 import requests
 
 app = Flask(__name__)
 
+REMOTE_HOST = os.getenv("NOTIFIER_PROXY_HOST", "192.168.1.192")
+REMOTE_PORT = os.getenv("NOTIFIER_PROXY_PORT", "6300")
+
 
 @app.route('/notify')
 def notify():
     requests.get(
-        "http://192.168.1.192:6300/notify", params=request.args)
+        "http://{}:{}/notify".format(
+            REMOTE_HOST, REMOTE_PORT), params=request.args)
     return "OK"
